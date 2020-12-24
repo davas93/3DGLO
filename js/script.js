b61ad7d1de0899f9a8370b33eadb065234bffaf2
davas93 <vasin.da93@gmail.com>
2020-12-24 17:21:14 +0300

    correct postData

--- js/script.js
+++ js/script.js
@@ -337,31 +337,43 @@ window.addEventListener('DOMContentLoaded', () => {
 		form.addEventListener('submit', (event) => {
 			event.preventDefault();
 			form.appendChild(statusMessage);
+			statusMessage.textContent = loadMessage;
 
-			const request = new XMLHttpRequest();
-			request.addEventListener('readystatechange', () => {
-				statusMessage.textContent = loadMessage;
-
-				if (request.readyState !== 4) {
-					return;
-				}
-				if (request.status === 200) {
-					statusMessage.textContent = successMessage;
-				} else {
-					statusMessage.textContent = errorMessage;
-				}
-			});
-			request.open('POST', '../server.php');
-			request.setRequestHeader('Content-Type', 'application/json');
 			const formData = new FormData(form);
 			let body = {};
 
 			formData.forEach((val, key) => {
 				body[key] = val;
 			});
+			postData(
+				body,
+				() => {
+					statusMessage.textContent = successMessage;
+				},
+				(error) => {
+					statusMessage.textContent = errorMessage;
+					console.error(error);
+				}
+			);
+		});
+
+		const postData = (body, outputData, errorData) => {
+			const request = new XMLHttpRequest();
+			request.addEventListener('readystatechange', () => {
+				if (request.readyState !== 4) {
+					return;
+				}
+				if (request.status === 200) {
+					outputData();
+				} else {
+					errorData(request.status);
+				}
+			});
+			request.open('POST', '../server.php');
+			request.setRequestHeader('Content-Type', 'application/json');
 
 			request.send(JSON.stringify(body));
-		});
+		};
 	};
 	sendForm();
 });
