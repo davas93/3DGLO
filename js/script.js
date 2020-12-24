fe7fd6c4ef38f404fbcaacc5d10bbda25a00f1f3
davas93 <vasin.da93@gmail.com>
2020-12-24 19:43:18 +0300

    complete

--- js/script.js
+++ js/script.js
@@ -327,6 +327,17 @@ window.addEventListener('DOMContentLoaded', () => {
 	//validate inputs
 	const validInputs = () => {
 		maskPhone('input[name = user_phone]');
+
+		document.addEventListener('input', (event) => {
+			let target = event.target;
+
+			if (
+				target.matches('input[name = user_message]') ||
+				target.matches('input[name = user_name]')
+			) {
+				target.value = target.value.replace(/[^а-яА-Я\s]+/g, '');
+			}
+		});
 	};
 	validInputs();
 
