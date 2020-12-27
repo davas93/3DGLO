f00dbd07b0dd96a6818520dc991bfff00bcd3778
davas93 <vasin.da93@gmail.com>
2020-12-27 19:52:29 +0300

    checkpoint1

--- js/script.js
+++ js/script.js
@@ -1,155 +1,154 @@
-window.addEventListener('DOMContentLoaded', () => {
-	'use strict';
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
 
-	// Timer
-	const countTimer = (deadline) => {
-		const timerNumbers = document.querySelector('#timer'),
-			timerHours = document.querySelector('#timer-hours'),
-			timerMinutes = document.querySelector('#timer-minutes'),
-			timerSeconds = document.querySelector('#timer-seconds');
+  // Timer
+  const countTimer = (deadline) => {
+    const timerNumbers = document.querySelector("#timer"),
+      timerHours = document.querySelector("#timer-hours"),
+      timerMinutes = document.querySelector("#timer-minutes"),
+      timerSeconds = document.querySelector("#timer-seconds");
 
-		const getTimeremaining = () => {
-			const dateStop = new Date(deadline).getTime(),
-				dateNow = new Date().getTime(),
-				timeRemaining = (dateStop - dateNow) / 1000,
-				seconds = Math.floor(timeRemaining % 60),
-				minutes = Math.floor((timeRemaining / 60) % 60),
-				hours = Math.floor(timeRemaining / 60 / 60);
-			return { timeRemaining, hours, minutes, seconds };
-		};
+    const getTimeremaining = () => {
+      const dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+      return { timeRemaining, hours, minutes, seconds };
+    };
 
-		const updateClock = () => {
-			const timer = getTimeremaining();
+    const updateClock = () => {
+      const timer = getTimeremaining();
 
-			if (timer.hours < 10) {
-				timerHours.textContent = '0' + timer.hours;
-			} else {
-				timerHours.textContent = timer.hours;
-			}
+      if (timer.hours < 10) {
+        timerHours.textContent = "0" + timer.hours;
+      } else {
+        timerHours.textContent = timer.hours;
+      }
 
-			if (timer.minutes < 10) {
-				timerMinutes.textContent = '0' + timer.minutes;
-			} else {
-				timerMinutes.textContent = timer.minutes;
-			}
+      if (timer.minutes < 10) {
+        timerMinutes.textContent = "0" + timer.minutes;
+      } else {
+        timerMinutes.textContent = timer.minutes;
+      }
 
-			if (timer.seconds < 10) {
-				timerSeconds.textContent = '0' + timer.seconds;
-			} else {
-				timerSeconds.textContent = timer.seconds;
-			}
+      if (timer.seconds < 10) {
+        timerSeconds.textContent = "0" + timer.seconds;
+      } else {
+        timerSeconds.textContent = timer.seconds;
+      }
 
-			if (timer.timeRemaining <= 0) {
-				timerHours.textContent = '00';
-				timerMinutes.textContent = '00';
-				timerSeconds.textContent = '00';
-				timerNumbers.style.color = 'red';
-				clearInterval(idInterval);
-			}
-		};
+      if (timer.timeRemaining <= 0) {
+        timerHours.textContent = "00";
+        timerMinutes.textContent = "00";
+        timerSeconds.textContent = "00";
+        timerNumbers.style.color = "red";
+        clearInterval(idInterval);
+      }
+    };
 
-		const idInterval = setInterval(updateClock, 1000);
-	};
-	countTimer('18 December 2020');
+    const idInterval = setInterval(updateClock, 1000);
+  };
+  countTimer("31 December 2020");
 
-	//Menu
-	const toggleMenu = () => {
-		const btnMenu = document.querySelector('.menu'),
-			menu = document.querySelector('menu');
+  //Menu
+  const toggleMenu = () => {
+    const menu = document.querySelector("menu");
 
-		const handlerMenu = () => {
-			menu.classList.toggle('active-menu');
-		};
+    const handlerMenu = () => {
+      menu.classList.toggle("active-menu");
+    };
 
-		btnMenu.addEventListener('click', handlerMenu);
-		menu.addEventListener('click', (event) => {
-			let target = event.target;
-			if (target.classList.contains('close-btn')) {
-				handlerMenu();
-			} else {
-				target = event.target.closest('li');
+    document.addEventListener("click", (event) => {
+      let target = event.target;
 
-				if (!target) return;
-				if (!menu.contains(target)) return;
-				handlerMenu();
-			}
-		});
-	};
-	toggleMenu();
+      if (
+        target.closest(".menu") ||
+        target.matches(".close-btn") ||
+        target.closest("menu>ul>li")
+      ) {
+        handlerMenu();
+      } else if (!target.matches(".active-menu")) {
+        menu.classList.remove("active-menu");
+      }
+    });
+  };
+  toggleMenu();
 
-	//Popup
-	const togglePopup = () => {
-		const popup = document.querySelector('.popup'),
-			popupBtns = document.querySelectorAll('.popup-btn'),
-			popupContent = document.querySelector('.popup-content');
-		let count = -500;
+  //Popup
+  const togglePopup = () => {
+    const popup = document.querySelector(".popup"),
+      popupBtns = document.querySelectorAll(".popup-btn"),
+      popupContent = document.querySelector(".popup-content");
+    let count = -500;
 
-		const animatePopup = () => {
-			if (window.outerWidth >= 768) {
-				popupContent.style.transform = `translateX(${count}px)`;
-				count += 20;
-				if (count < 0) {
-					requestAnimationFrame(animatePopup);
-				} else {
-					count = -500;
-					cancelAnimationFrame(animatePopup);
-				}
-			}
-		};
+    const animatePopup = () => {
+      if (window.outerWidth >= 768) {
+        popupContent.style.transform = `translateX(${count}px)`;
+        count += 20;
+        if (count < 0) {
+          requestAnimationFrame(animatePopup);
+        } else {
+          count = -500;
+          cancelAnimationFrame(animatePopup);
+        }
+      }
+    };
 
-		popupBtns.forEach((elem) => {
-			elem.addEventListener('click', () => {
-				popup.style.display = 'block';
-				requestAnimationFrame(animatePopup);
-			});
-		});
+    popupBtns.forEach((elem) => {
+      elem.addEventListener("click", () => {
+        popup.style.display = "block";
+        requestAnimationFrame(animatePopup);
+      });
+    });
 
-		popup.addEventListener('click', (event) => {
-			let target = event.target;
+    popup.addEventListener("click", (event) => {
+      let target = event.target;
 
-			if (target.classList.contains('popup-close')) {
-				popup.style.display = 'none';
-			} else {
-				target = target.closest('.popup-content');
+      if (target.classList.contains("popup-close")) {
+        popup.style.display = "none";
+      } else {
+        target = target.closest(".popup-content");
 
-				if (!target) {
-					popup.style.display = 'none';
-				}
-			}
-		});
-	};
-	togglePopup();
+        if (!target) {
+          popup.style.display = "none";
+        }
+      }
+    });
+  };
+  togglePopup();
 
-	//Tabs
-	const tabs = () => {
-		const tabHeader = document.querySelector('.service-header'),
-			tab = tabHeader.querySelectorAll('.service-header-tab'),
-			tabContent = document.querySelectorAll('.service-tab');
+  //Tabs
+  const tabs = () => {
+    const tabHeader = document.querySelector(".service-header"),
+      tab = tabHeader.querySelectorAll(".service-header-tab"),
+      tabContent = document.querySelectorAll(".service-tab");
 
-		const toggleTabContent = (index) => {
-			for (let i = 0; i < tabContent.length; i++) {
-				if (index === i) {
-					tab[i].classList.add('active');
-					tabContent[i].classList.remove('d-none');
-				} else {
-					tab[i].classList.remove('active');
-					tabContent[i].classList.add('d-none');
-				}
-			}
-		};
+    const toggleTabContent = (index) => {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add("active");
+          tabContent[i].classList.remove("d-none");
+        } else {
+          tab[i].classList.remove("active");
+          tabContent[i].classList.add("d-none");
+        }
+      }
+    };
 
-		tabHeader.addEventListener('click', (event) => {
-			let target = event.target;
-			target = target.closest('.service-header-tab');
+    tabHeader.addEventListener("click", (event) => {
+      let target = event.target;
+      target = target.closest(".service-header-tab");
 
-			if (target) {
-				tab.forEach((item, i) => {
-					if (item === target) {
-						toggleTabContent(i);
-					}
-				});
-			}
-		});
-	};
-	tabs();
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toggleTabContent(i);
+          }
+        });
+      }
+    });
+  };
+  tabs();
 });
